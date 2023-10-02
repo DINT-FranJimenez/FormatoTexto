bc496d14b8888e0c7b40f55a1ede19a61a12031b
FRANCISCO JIMÉNEZ LOZANO <frajimloz@alu.edu.gva.es>
2023-10-02 19:29:03 +0200

    Agregar archivos de proyecto.

---
+++ Tema2-FormatoTexto/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema2_FormatoTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EscribeTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            palabaraTextBlock.Text = escribeTextBox.Text;
+        }
+
+        private void NegritaCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+             palabaraTextBlock.FontWeight = FontWeights.Bold;
+        }
+
+        private void NegritaCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            palabaraTextBlock.FontWeight = FontWeights.Normal;
+        }
+
+
+        private void CursivaCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            palabaraTextBlock.FontStyle = FontStyles.Italic;
+        }
+
+        private void CursivaCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            palabaraTextBlock.FontStyle = FontStyles.Normal;
+        }
+
+        private void AzulRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+            if(azulRadioButton.IsChecked == true)
+                palabaraTextBlock.Foreground = Brushes.Blue;
+        }
+
+        private void RojoRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (rojoRadioButton.IsChecked == true )
+                palabaraTextBlock.Foreground = Brushes.Red;
+        }
+
+        private void VerdeRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (verdeRadioButton.IsChecked == true)
+                palabaraTextBlock.Foreground = Brushes.Green;
+        }
+    }
+}
